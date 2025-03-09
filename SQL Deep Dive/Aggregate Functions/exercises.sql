ed73d74e8aa40fc81722882f4605dc4a5fb7f580
Osvaldo Robles <117135874+mrzelbor@users.noreply.github.com>
2025-03-09 02:39:49 -0600

    Update exercises.sql

--- SQL Deep Dive/Aggregate Functions/exercises.sql
+++ SQL Deep Dive/Aggregate Functions/exercises.sql
@@ -8,11 +8,15 @@
 
 -- Question 1: What is the average salary for the company?
 -- Table: Salaries
-
+SELECT 
+    AVG(salary)
+FROM salaries;
 
 -- Question 2: What year was the youngest person born in the company?
 -- Table: employees
-
+SELECT 
+    AVG(salary)
+FROM salaries;
 
 -- BEFORE YOU START
 /*
@@ -23,7 +27,9 @@
 
 -- Question 1: How many towns are there in france?
 -- Table: Towns
-
+SELECT
+    COUNT(*)
+FROM towns
 
 -- BEFORE YOU START
 /*
@@ -34,11 +40,22 @@
 
 -- Question 1: How many official languages are there?
 -- Table: countrylanguage
+SELECT
+    COUNT(isofficial)
+FROM countrylanguage
+WHERE isofficial IS TRUE;
 
 -- Question 2: What is the average life expectancy in the world?
 -- Table: country
+SELECT
+    AVG(lifeexpectancy)
+FROM country;
 
 -- Question 3: What is the average population for cities in the netherlands?
 -- Table: city
+SELECT
+    AVG(population)
+FROM city
+WHERE "countrycode" = 'NLD';
 
 
