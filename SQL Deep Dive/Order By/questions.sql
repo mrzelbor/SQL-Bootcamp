c7ec71f6082c42068c2a1bec8aa02ddf01829cd5
Osvaldo Robles <117135874+mrzelbor@users.noreply.github.com>
2025-03-09 15:34:29 -0600

    Update questions.sql

--- SQL Deep Dive/Order By/questions.sql
+++ SQL Deep Dive/Order By/questions.sql
@@ -3,17 +3,28 @@
 * Table: employees
 * Question: Sort employees by first name ascending and last name descending
 */
-
+SELECT
+    *
+FROM employees
+ORDER BY first_name, last_name DESC;
 
 /*
 * DB: Employees
 * Table: employees
 * Question: Sort employees by age
 */
-
+SELECT
+    AGE(birth_date), *
+FROM employees
+ORDER BY age;
 
 /*
 * DB: Employees
 * Table: employees
 * Question: Sort employees who's name starts with a "k" by hire_date
 */
+SELECT
+    *
+FROM employees
+WHERE first_name ILIKE 'K%'
+ORDER BY hire_date;
