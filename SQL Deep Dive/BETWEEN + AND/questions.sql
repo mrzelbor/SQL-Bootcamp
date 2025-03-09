e0d86dc58f9b35b84e79c22a4a333edad12baf88
Osvaldo Robles <117135874+mrzelbor@users.noreply.github.com>
2025-03-09 03:16:48 -0600

    Update questions.sql

--- SQL Deep Dive/BETWEEN + AND/questions.sql
+++ SQL Deep Dive/BETWEEN + AND/questions.sql
@@ -4,8 +4,17 @@
 /*
 * Write your query here
 */
+SELECT 
+    *
+from customers
+where age BETWEEN 30 AND 50
+AND income < 50000;
 
 -- What is the average income between the ages of 20 and 50? (Including 20 and 50)
 /*
 * Write your query here
 */
+SELECT 
+    AVG(income)
+from customers
+where age BETWEEN 20 AND 50;
