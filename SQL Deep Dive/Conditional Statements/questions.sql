ff6958f984a09221590bbd8caebaad240701d6b2
Osvaldo Robles <117135874+mrzelbor@users.noreply.github.com>
2025-03-16 18:35:18 -0600

    Conditional Statements questions.sql

--- SQL Deep Dive/Conditional Statements/questions.sql
+++ SQL Deep Dive/Conditional Statements/questions.sql
@@ -5,3 +5,14 @@
 * if it's between 10 and 20 you show 'average' 
 * and of is lower than or equal to 10 you show 'cheap'.
 */
+SELECT 
+    title
+    ,actor
+    ,price
+    ,CASE
+        WHEN price > 20.00 THEN 'EXPENSIVE'
+        WHEN price BETWEEN 10.00 AND 20.00 THEN 'AVERAGE'
+        WHEN price <= 10.00 THEN 'CHEAP'
+        ELSE 'na'
+    END AS "Price Class"
+FROM products
